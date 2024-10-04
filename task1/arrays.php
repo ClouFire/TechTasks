2b95c8747ab2e7487772839529175b6ac8d03f52
ClouFire <serrg8425@gmail.com>
2024-10-04 16:17:01 +0300

    Minor changes (created function to show the table)

--- task1/arrays.php
+++ task1/arrays.php
@@ -1,4 +1,6 @@
-
+<?php
+require_once __DIR__ . '/data.php';
+?>
 
 <!doctype html>
 <html lang="en">
@@ -7,7 +9,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>1st-task</title>
 </head>
 <body>
@@ -16,26 +19,9 @@
             <tr>
                 <td></td>
                 <?php
-                require_once __DIR__ . '/data.php';
-                foreach (array_unique($subjects) as $subject) {
-                    echo "<td>$subject</td>";
-                }
-                foreach ($grades as $name => $values) {
-                    echo '<tr>' . "<td>$name</td>";
-                    foreach ($values as $subject => $grade) {
-
-                        if ($grade > 0) {
-                            echo "<td>$grade</td>";
-                        }
-                        else {
-                            echo "<td></td>";
-                        }
-                    }
-                } ?>
-            </tr>
-
+                showTable($grades, $subjects);
+                ?>
         </table>
     </div>
-    <a href="/index.php">Назад</a>
 </body>
 </html>
--- task1/data.php
+++ task1/data.php
@@ -9,16 +9,34 @@ $data = [
     ['Петров', 'ОБЖ', 4],
 ];
 
-$names = array_column($data, '0');
-$subjects = array_column($data, '1');
+sort($data);
+$subjects = array_unique(array_column($data, 1));
+sort($subjects);
 $grades = [];
-array_multisort($subjects, SORT_STRING, $names, SORT_STRING, $data);
 
-foreach (array_unique($names) as $name) {
-    foreach (array_unique($subjects) as $subject) {
-        $grades[$name][$subject] = 0;
+foreach($data as list($name, $subject, $grade)) {
+    if(isset($grades[$name][$subject])) {
+        $grades[$name][$subject] += $grade;
+    }
+    else {
+        $grades[$name][$subject] = $grade;
     }
 }
-foreach ($data as list($name, $subject, $grade)) {
-    $grades[$name][$subject] += $grade;
+
+function showTable($grades, $subjects) {
+    foreach($subjects as $subject) {
+        echo "<td>$subject</td>";
+    }
+    foreach($grades as $name => $info) {
+        echo "<tr>" . "<td>$name</td>";
+        foreach($subjects as $subject) {
+            if(isset($info[$subject])) {
+                echo "<td>$info[$subject]</td>";
+            }
+            else {
+                echo "<td></td>";
+            }
+        }
+        echo "</tr>";
+    }
 }
