fdbd3729089c16d8df77967dba3dac457b756fb0
ClouFire <serrg8425@gmail.com>
2024-10-04 17:56:30 +0300

    Fully restructured approach. Added Singleton DB connection.

---
+++ task2/Database.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once __DIR__ . '/config.php';
+
+class DB extends data
+{
+    private static $instance = null;
+
+    private $connection = null;
+
+    protected function __construct() {
+        $this->connection = new \PDO
+        (
+            "mysql:host=$this->dbHost;dbname=$this->dbName;charset=utf8",
+            "$this->dbLogin",
+            "$this->dbPass",
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false,
+            ]
+        );
+    }
+
+    protected function __clone()
+    {
+    }
+
+    public function __wakeup()
+    {
+        throw new \BadMethodCallException('Unable to deserialize database connection');
+    }
+
+    public static function getInstance(): DB
+    {
+        if(null === self::$instance) {
+            return self::$instance = new static();
+        }
+
+        return self::$instance;
+    }
+
+    public static function connection(): \PDO
+    {
+        return static::getInstance()->connection;
+    }
+
+    public static function prepare($statement): \PDOStatement
+    {
+        return static::connection()->prepare($statement);
+    }
+
+}
---
+++ task2/SQL.php
@@ -0,0 +1,32 @@
+<?php
+require_once __DIR__ . "/Database.php";
+
+$query = DB::prepare
+    ("
+    DELETE categories FROM categories 
+    LEFT JOIN products ON products.category_id = categories.id
+    WHERE products.category_id IS NULL
+    
+    ");
+
+$query->execute();
+
+$query = DB::prepare
+    ("
+    DELETE products FROM products 
+    LEFT JOIN availabilities ON products.id = availabilities.product_id 
+    WHERE availabilities.product_id IS NULL
+    ");
+
+$query->execute();
+
+$query = DB::prepare
+    ("
+    DELETE stocks FROM stocks 
+    LEFT JOIN availabilities ON stocks.id = availabilities.stock_id    
+    WHERE availabilities.stock_id IS NULL
+    ");
+
+$query->execute();
+
+
