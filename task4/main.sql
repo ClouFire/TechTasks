e5ad22c72336c82962272021cd5ff5a4d86e0b34
ClouFire <serrg8425@gmail.com>
2024-10-07 16:58:09 +0300

    restructured old SQL to make it possible to work with new code

--- task4/main.sql
+++ task4/main.sql
@@ -2,9 +2,9 @@ CREATE DATABASE users;
 
 CREATE TABLE IF NOT EXISTS 'users'.'comments' (
     'id' BIGINT NOT NULL AUTO_INCREMENT,
-    'username' VARCHAR(225) NOT NULL DEFAULT 'Аноним',
+    'username' VARCHAR(30) NOT NULL DEFAULT 'Аноним',
+    'date' TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP(),
     'comment' TEXT NOT NULL,
-    'date' DATETIME NOT NULL,
     PRIMARY KEY ('id')
 )
 ENGINE = InnoDB;
