d8c8c30b77ef79efb4d78e7fd06beffa3f8fad73
ClouFire <serrg8425@gmail.com>
2024-10-07 16:57:10 +0300

    added "addComment" w/o username

--- task3/add-comment.php
+++ task3/add-comment.php
@@ -3,13 +3,22 @@ require_once __DIR__ . '/Database.php';
 require_once __DIR__ . '/csrf.php';
 function addComment($username, $comment) {
     checkCSRF($_POST['csrf_token']);
+    if($username) {
+        $query = DB::prepare("INSERT INTO users(username, comment, date) VALUES (:username, :comment, :date)");
+        $query->execute([
+            'username' => $username,
+            'comment' => htmlspecialchars($comment),
+            'date' => date('Y-m-d H:i:s', time()),
+        ]);
+    }
+    else {
+        $query = DB::prepare("INSERT INTO users(comment, date) VALUES (:comment, :date)");
+        $query->execute([
+            'comment' => htmlspecialchars($comment),
+            'date' => date('Y-m-d H:i:s', time()),
+        ]);
+    }
 
-    $query = DB::prepare("INSERT INTO users(username, comment, date) VALUES (:username, :comment, :date)");
-    $query->execute([
-        'username' => $username,
-        'comment' => htmlspecialchars($comment),
-        'date' => date('Y-m-d H:i:s', time()),
-    ]);
     return True;
 }
 
