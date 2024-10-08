c9ed1a52e8de5b3257b7baf538c75b9ab889da2d
ClouFire <serrg8425@gmail.com>
2024-10-08 10:53:28 +0300

    getSistersAmount v2.0

--- task5/main.php
+++ task5/main.php
@@ -1,7 +1,16 @@
 <?php
 
-function getSistersAmount($n, $m) {
-    return $n+1;
-}
+function getSistersAmount($n, $m): int {
+    if ($n > 0 and $m > 0) {
+        return $n+1;
+    }
+    else {
+        throw new Exception('Отрицательное значение');
+    }
 
-print(getSistersAmount(1, 2));
+}
+try {
+    print(getSistersAmount(1, 2));
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
