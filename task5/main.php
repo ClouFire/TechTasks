daa7e44f7b5ce15c387c375354168b360393c28f
ClouFire <serrg8425@gmail.com>
2024-10-08 10:58:50 +0300

    Minor fix

--- task5/main.php
+++ task5/main.php
@@ -1,6 +1,12 @@
 <?php
 
-function getSistersAmount($n, $m): int {
+/**
+ * @param int $n количество сестер Алисы
+ * @param int $m количество братьев Алисы
+ * @return int количество систер у произвольного брата Алисы
+ * @throw Exception отрицательное значение братьев\сестер Алисы
+*/
+function getSistersAmount(int $n, int $m): int {
     if ($n > 0 and $m > 0) {
         return $n+1;
     }
