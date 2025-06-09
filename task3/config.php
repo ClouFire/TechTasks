ace89b5c46eb66d6b760f3f1c1a42a7d5e705db8
cloufire <sereg-1910@mail.ru>
2025-06-09 18:51:23 +0300

    Загрузил конфиг, т.к. без него работать не будет, увы :(

---
+++ task3/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define("ROOT", dirname(__DIR__) . '\task3_how_i_see');
+
+const DATABASE = ROOT . '/DataBase.php';
+const HELPERS = ROOT . '/helpers.php';
+const DBHOST = '';
+const DBNAME = 'task3';
+const DBPASS = '';
+const DBLOGIN = 'root';
